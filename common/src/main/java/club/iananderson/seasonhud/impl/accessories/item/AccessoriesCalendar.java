0278003adf93f0bd0fbd339dd044f9af26dfdd4f
Ian Anderson <lanAnderson@users.noreply.github.com>
2024-12-24 12:29:09 -0700

    Updated to latest Accessories

--- common/src/main/java/club/iananderson/seasonhud/impl/accessories/item/AccessoriesCalendar.java
+++ common/src/main/java/club/iananderson/seasonhud/impl/accessories/item/AccessoriesCalendar.java
@@ -2,15 +2,16 @@ package club.iananderson.seasonhud.impl.accessories.item;
 
 import club.iananderson.seasonhud.platform.Services;
 import com.mojang.blaze3d.vertex.PoseStack;
-import io.wispforest.accessories.api.AccessoriesAPI;
 import io.wispforest.accessories.api.Accessory;
+import io.wispforest.accessories.api.AccessoryRegistry;
 import io.wispforest.accessories.api.client.AccessoriesRendererRegistry;
 import io.wispforest.accessories.api.client.AccessoryRenderer;
 import io.wispforest.accessories.api.client.SimpleAccessoryRenderer;
 import io.wispforest.accessories.api.slot.SlotReference;
 import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.model.HumanoidModel;
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.client.renderer.entity.state.HumanoidRenderState;
+import net.minecraft.client.renderer.entity.state.LivingEntityRenderState;
 import net.minecraft.world.item.ItemStack;
 
 public class AccessoriesCalendar implements Accessory {
@@ -22,15 +23,15 @@ public class AccessoriesCalendar implements Accessory {
   }
 
   public static void init() {
-    AccessoriesAPI.registerAccessory(Services.SEASON.calendar(), new AccessoriesCalendar());
+    AccessoryRegistry.register(Services.SEASON.calendar(), new AccessoriesCalendar());
   }
 
   public static class Renderer implements SimpleAccessoryRenderer {
 
     @Override
-    public <M extends LivingEntity> void align(ItemStack stack, SlotReference reference, EntityModel<M> model,
-        PoseStack matrices) {
-      if (!(model instanceof HumanoidModel<? extends LivingEntity> humanoidModel)) {
+    public <M extends LivingEntityRenderState> void align(ItemStack stack, SlotReference reference,
+        EntityModel<M> model, M renderState, PoseStack matrices) {
+      if (!(model instanceof HumanoidModel<? extends HumanoidRenderState> humanoidModel)) {
         return;
       }
 
